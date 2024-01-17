b79941f099097bc9cfce319bb2ee8aea01b9c4c9
Chiku Okechukwu <tuo62572@temple.edu>
2024-01-17 10:36:06 -0500

    newer changes

--- random.c
+++ random.c
@@ -4,7 +4,6 @@
 
 char randchar() {
 // uses the random function to print a random lowercase letter from the alphabet(26)
-    srand( (unsigned)time(NULL) );
     char letter_one = 'a' + (random() % 26);
 
 
